5b04fcccf6e53f3dfd7cee8bfa1c852f07510c7b
Lucas <13675790+lschiavini@users.noreply.github.com>
2022-02-28 17:54:54 -0300

    Init test with issue bug

--- app/src/main/java/com/lucas/schiavini/movieapp/model/MovieDatabase.kt
+++ app/src/main/java/com/lucas/schiavini/movieapp/model/MovieDatabase.kt
@@ -1,30 +1,31 @@
 package com.lucas.schiavini.movieapp.model
-
-import android.content.Context
-import androidx.room.Database
-import androidx.room.Room
-import androidx.room.RoomDatabase
-
-
-@Database(entities = [Movie::class], version = 1)
-abstract class MovieDatabase : RoomDatabase() {
-    abstract fun movieDao(): MovieDao
-
-    companion object {
-        @Volatile
-        private var instance: MovieDatabase? = null
-        private val LOCK = Any()
-
-        operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
-            instance ?: buildDatabase(context).also {
-                instance = it
-            }
-        }
-
-        private fun buildDatabase(context: Context) = Room.databaseBuilder(
-            context.applicationContext,
-            MovieDatabase::class.java,
-            "moviedatabase"
-        ).build()
-    }
-}
+//
+//import android.content.Context
+//import androidx.room.Database
+//import androidx.room.Room
+//import androidx.room.RoomDatabase
+//import com.lucas.schiavini.client.model.Movie
+//
+//
+//@Database(entities = [Movie::class], version = 1)
+//abstract class MovieDatabase : RoomDatabase() {
+//    abstract fun movieDao(): MovieDao
+//
+//    companion object {
+//        @Volatile
+//        private var instance: MovieDatabase? = null
+//        private val LOCK = Any()
+//
+//        operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
+//            instance ?: buildDatabase(context).also {
+//                instance = it
+//            }
+//        }
+//
+//        private fun buildDatabase(context: Context) = Room.databaseBuilder(
+//            context.applicationContext,
+//            MovieDatabase::class.java,
+//            "moviedatabase"
+//        ).build()
+//    }
+//}
